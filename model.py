6dd01cdabeaeaf94956d94c83764b36a5c566584
Moonghosty <128601188+Moonghosty@users.noreply.github.com>
2024-11-08 08:59:39 -0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,9 @@
+class tarefas: 
+    def __init__(self, id, titulo, descricao, data):
+        self.id = id
+        self.titulo = titulo
+        self.descricao = descricao
+        self.data = data
+
+lista_tarefas = []
+        
